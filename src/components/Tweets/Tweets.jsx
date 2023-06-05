86933b9b1573aeb6833cf7db8129297593b8383c
Inna Pokydko <inna.pokydko@gmail.com>
2023-06-05 13:21:02 +0300

    fix mistake

--- src/components/Tweets/Tweets.jsx
+++ src/components/Tweets/Tweets.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { FcHome } from 'react-icons/fc';
 import { useGetUsersQuery } from 'redux/auth/operations';
 import TweetCard from 'components/TweetCard/TweetCard';
@@ -37,16 +37,21 @@ const Tweets = () => {
     localStorage.setItem('page', page.toString());
   }, [filter, page]);
 
+  const filterUsers = useCallback((users) => {
+    if (filter === 'follow') {
+      return users.filter(user => !user.followStatus);
+    } else if (filter === 'following') {
+      return users.filter(user => user.followStatus);
+    }
+    return users;
+  }, [filter]);
+
   useEffect(() => {
     if (users) {
       const filtered = filterUsers(users);
       setFilteredUsers(filtered);
     }
-  }, [users, filter]);
-
-  useEffect(() => {
-    setPage(1);
-  }, [filter]);
+  }, [users, filter, filterUsers]);
 
   if (isLoading && page === 1) {
     return <Loader>Loading...</Loader>;
@@ -58,15 +63,6 @@ const Tweets = () => {
 
   const handleLoadMore = () => {
     setPage(prevPage => prevPage + 1);
-  };
-
-  const filterUsers = (users) => {
-    if (filter === 'follow') {
-      return users.filter(user => !user.followStatus);
-    } else if (filter === 'following') {
-      return users.filter(user => user.followStatus);
-    }
-    return users;
   };
 
   const displayedUsers = filteredUsers.slice(0, page * PER_PAGE);
@@ -111,32 +107,63 @@ export default Tweets;
 
 
 
-// import React, { useState, useEffect} from 'react';
+// import React, { useState, useEffect } from 'react';
 // import { FcHome } from 'react-icons/fc';
 // import { useGetUsersQuery } from 'redux/auth/operations';
 // import TweetCard from 'components/TweetCard/TweetCard';
+// import Loader from 'components/Loader';
 // import {
-//   LoadingMessage,
 //   ErrorMessage,
 //   LoadMoreButton,
 //   BackLink,
 //   TweetsContainer,
 //   CardContainer,
 //   Section,
+//   FilterDropdown,
 // } from './Tweets.styled';
 
 // const PER_PAGE = 12;
 
 // const Tweets = () => {
 //   const [page, setPage] = useState(1);
-//   const { data: users, error, isLoading, isFetching } = useGetUsersQuery(page);
+//   const [filter, setFilter] = useState('all');
+//   const { data: users, error, isLoading } = useGetUsersQuery(page);
+//   const [filteredUsers, setFilteredUsers] = useState([]);
 
 //   useEffect(() => {
-//     setPage(1);
+//     const savedFilter = localStorage.getItem('filter');
+//     const savedPage = localStorage.getItem('page');
+//     if (savedFilter) {
+//       setFilter(savedFilter);
+//     }
+//     if (savedPage) {
+//       setPage(parseInt(savedPage));
+//     }
 //   }, []);
 
+//   useEffect(() => {
+//     localStorage.setItem('filter', filter);
+//     localStorage.setItem('page', page.toString());
+//   }, [filter, page]);
+
+//   useEffect(() => {
+//     const filterUsers = (users) => {
+//       if (filter === 'follow') {
+//         return users.filter(user => !user.followStatus);
+//       } else if (filter === 'following') {
+//         return users.filter(user => user.followStatus);
+//       }
+//       return users;
+//     };
+  
+//     if (users) {
+//       const filtered = filterUsers(users);
+//       setFilteredUsers(filtered);
+//     }
+//   }, [users, filter, filterUsers]);
+
 //   if (isLoading && page === 1) {
-//     return <LoadingMessage>Loading...</LoadingMessage>;
+//     return <Loader>Loading...</Loader>;
 //   }
 
 //   if (error && page === 1) {
@@ -147,7 +174,17 @@ export default Tweets;
 //     setPage(prevPage => prevPage + 1);
 //   };
 
-//   const displayedUsers = users ? users.slice(0, page * PER_PAGE) : [];
+//   const filterUsers = (users) => {
+//     if (filter === 'follow') {
+//       return users.filter(user => !user.followStatus);
+//     } else if (filter === 'following') {
+//       return users.filter(user => user.followStatus);
+//     }
+//     return users;
+//   };
+
+//   const displayedUsers = filteredUsers.slice(0, page * PER_PAGE);
+//   const isLoadMoreVisible = displayedUsers.length < filteredUsers.length && displayedUsers.length > 0;
 
 //   return (
 //     <Section>
@@ -156,12 +193,20 @@ export default Tweets;
 //         Back to home
 //       </BackLink>
 //       <CardContainer>
+//         <FilterDropdown
+//           value={filter}
+//           onChange={(e) => setFilter(e.target.value)}
+//         >
+//           <option value="all">Show All</option>
+//           <option value="follow">Follow</option>
+//           <option value="following">Following</option>
+//         </FilterDropdown>
 //         <TweetsContainer>
 //           {displayedUsers.map(user => (
 //             <TweetCard key={user.id} user={user} />
 //           ))}
 //         </TweetsContainer>
-//         {!isFetching && displayedUsers.length < users.length && (
+//         {isLoadMoreVisible && (
 //           <LoadMoreButton onClick={handleLoadMore}>
 //             Load More
 //           </LoadMoreButton>
@@ -172,3 +217,11 @@ export default Tweets;
 // };
 
 // export default Tweets;
+
+
+
+
+
+
+
+
